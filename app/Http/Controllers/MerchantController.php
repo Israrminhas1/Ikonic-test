b311bbe5b88905c18ab88894a20503fa8d005516
Israr Minhas <israrminhas99@gmail.com>
2023-11-25 15:19:37 +0500

    test cleared

---
+++ app/Http/Controllers/MerchantController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Merchant;
+use App\Models\Order;
+use App\Services\MerchantService;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Carbon;
+
+class MerchantController extends Controller
+{
+    public function __construct(
+        MerchantService $merchantService
+    ) {
+    }
+
+    /**
+     * Useful order statistics for the merchant API.
+     * 
+     * @param Request $request Will include a from and to date
+     * @return JsonResponse Should be in the form {count: total number of orders in range, commission_owed: amount of unpaid commissions for orders with an affiliate, revenue: sum order subtotals}
+     */
+    public function orderStats(Request $request): JsonResponse
+    {
+        // TODO: Complete this method
+        $dateRange = [
+            'from' => Carbon::parse($request->get('from')),
+            'to' => Carbon::parse($request->get('to')),
+        ];
+
+        $orders = Order::whereBetween('created_at', array_values($dateRange))->get();
+
+        $count = $orders->count();
+        $revenue = $orders->sum('subtotal');
+        $totalCommissions = $orders->where('payout_status', Order::STATUS_UNPAID)->sum('commission_owed');
+        $commissionsOwedByNone = $orders->whereNull('affiliate_id')->where('payout_status', Order::STATUS_UNPAID)->sum('commission_owed');
+
+        $responseData = [
+            "count" => $count,
+            "revenue" => $revenue,
+            "commissions_owed" => $totalCommissions - $commissionsOwedByNone,
+        ];
+
+        return response()->json($responseData);
+    }
+}
---
+++ database/migrations/2022_05_13_220658_create_affiliates_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('affiliates', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id');
+            $table->foreignId('merchant_id');
+            // TODO: Replace me with a brief explanation of why floats aren't the correct data type, and replace with the correct data type.
+            /* 
+            Using float for money can cause small errors in calculations due to how computers represent decimal numbers.
+            To avoid these errors, it's better to use decimal for storing money values in databases.
+            */
+            $table->decimal('commission_rate', 5, 1)->default(0.0);
+            $table->string('discount_code');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('affiliates');
+    }
+};
